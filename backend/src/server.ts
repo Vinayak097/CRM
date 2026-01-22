a6851e3a3fd4a3b609dcc23901680fbd284b728b
unknown <vinayak20injamure@gmail.com>
2026-01-22 11:52:26 +0530

    secure true

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -69,9 +69,9 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      maxAge: 24 * 60 * 60 * 1000, // 24 hours
+      secure: true,       // production
+      sameSite: "none",   // production
+      maxAge: 24 * 60 * 60 * 1000,
     },
   }),
 );
